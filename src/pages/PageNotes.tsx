eab56e074e7d9f30b0828f3b69a8df36b37ac155
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-15 01:26:15 +0200

    listing 2 books

--- src/pages/PageNotes.tsx
+++ src/pages/PageNotes.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import { BookNote } from "../types";
+import _bookNotes from '../data/bookNotes.json';
+
+const initialBookNotes: BookNote[] = _bookNotes as BookNote[];
+
 export const PageNotes = () => {
+	const [bookNotes] = useState<BookNote[]>(initialBookNotes);
+	
 	return (
 		<>
-		<p>This is the welcome page.</p>
-		<p><img src="images/books/shipAround.jpg"/></p>
+			<p>
+				<img src="images/books/shipAround.jpg" />
+			</p>
+			<hr />
+			<p>There are {bookNotes.length} book notes.</p>
 		</>
-	)
-}
+	);
+};
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type BookNote = {
+	dpodId: string;
+	idCode: string;
+	body: string;
+	page: number;
+	kind: string;
+};

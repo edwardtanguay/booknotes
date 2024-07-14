e66df297344a99d64cf531db3c50329c0ff457ab
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-15 00:38:28 +0200

    info page out

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,6 @@ export const Nav = () => {
 	return <nav>
 		<ul className="flex gap-4 bg-[#888] px-4 py-2 content rounded">
 			<li><NavLink to="/welcome">Welcome</NavLink></li>
-			<li><NavLink to="/info">Info</NavLink></li>
 			<li><NavLink to="/about">About</NavLink></li>
 		</ul>
 	</nav>;

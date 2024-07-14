77ce4f57bc4607a4cf9bd696f779c1a45466f91a
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-15 00:47:48 +0200

    picture

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
-			<h1 className="text-2xl md:text-3xl mb-3 text-[#333]">Book Notes</h1>
+			<h1 className="text-2xl md:text-3xl mb-3 text-[#333]">My Book Notes</h1>
 			<Nav/>
 		</>
 	);
--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -2,6 +2,13 @@ export const PageAbout = () => {
 	return (
 		<>
 			<p className="mb-3">
+				<a href="https://tanguay.info">
+					{" "}
+					<img
+						src="images/edward-tanguay.jpg"
+						className="w-[5rem] md:w-[10rem] float-right ml-3 mb-3 rounded"
+					/>
+				</a>
 				The book notes on this site are from books I am reading. Some of
 				them are direct quotes, some are my paraphrasing of the text,
 				and some of them are my ideas that I came to based on concepts
@@ -11,8 +18,8 @@ export const PageAbout = () => {
 				you want, you can find the text that my notes are based on.
 			</p>
 			<p>
-				The purpose of this site is not to sell anything or promote
-				any particular books or authors, but for me to interact with the
+				The purpose of this site is not to sell anything or promote any
+				particular books or authors, but for me to interact with the
 				texts I am reading and express my ideas about the concepts I
 				read as clearly as I can. I also post these quotes on social
 				media for discussion with others.

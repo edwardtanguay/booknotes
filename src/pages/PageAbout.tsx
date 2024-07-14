cdcf626df80b3933796ba17782ff24e4eb8582f7
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-15 00:40:52 +0200

    about text

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,22 @@
 export const PageAbout = () => {
 	return (
-		<p>This is the about page.</p>
-	)
-}
+		<>
+			<p className="mb-3">
+				The book notes on this site are from books I am reading. Some of
+				them are direct quotes, some are my paraphrasing of the text,
+				and some of them are my ideas that I came to based on concepts
+				read in the books. If a note is a direct quotation, I put it in
+				quotation marks, otherwise there are no quotation marks, but
+				each note has a reference to the book and page number so that if
+				you want, you can find the text that my notes are based on.
+			</p>
+			<p>
+				The purpose of this site is not to sell anything or promote
+				any particular books or authors, but for me to interact with the
+				texts I am reading and express my ideas about the concepts I
+				read as clearly as I can. I also post these quotes on social
+				media for discussion with others.
+			</p>
+		</>
+	);
+};
